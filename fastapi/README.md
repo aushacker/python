28fbc53b4ba2254f54eeee67c9ba95e339ef35c4
aushacker <25114336+aushacker@users.noreply.github.com>
2023-11-07 15:28:16 +1100

    python ci-pipeline working in tekton

--- fastapi/README.md
+++ fastapi/README.md
@@ -4,3 +4,5 @@ aushacker</br>
 November 2023
 
 A basic Python web server, containerised for easy Kubernetes deployment.
+
+Must manually add quay-push-secret to the target namespace.
--- fastapi/tools/10-build-server.sh
+++ fastapi/tools/10-build-server.sh
@@ -12,6 +12,7 @@ oc project $1
 echo "Building application"
 tkn pipeline start ci-pipeline \
   -w name=source,volumeClaimTemplateFile=tekton/volume-claim-template.yaml \
+  -w name=dockerconfig,secret=quay-push-secret \
   -p ocp-namespace="$NAMESPACE" \
   -p git-url=https://github.com/aushacker/python.git \
   -p git-revision=master \
